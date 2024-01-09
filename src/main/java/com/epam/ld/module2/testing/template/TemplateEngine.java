4b5d6ba0dce1f6ce9d63d99b1ed51221e14d1b2b
Andrei Klepets <ak1988.88@gmail.com>
2024-01-08 22:42:14 -0500

    implementation system should support values passed in runtime with #{...}

--- src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
+++ src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
@@ -18,12 +18,14 @@ public class TemplateEngine {
      */
     public String generateMessage(Template template, Client client) throws Exception {
         String templateString = template.getTemplateString();
+        String templateStringClean = template.getTemplateString();
 
         for (Map.Entry<String, String> entry : client.getVariables().entrySet()) {
             templateString = templateString.replace("#{"+entry.getKey()+"}", entry.getValue());
+            templateStringClean = templateString.replace(entry.getValue(), "");
         }
 
-        if(templateString.matches(".*#\\{\\w*\\}.*")) {
+        if(templateStringClean.matches(".*#\\{\\w*\\}.*")) {
             throw new IllegalArgumentException("some parameters missing");
         }
 
